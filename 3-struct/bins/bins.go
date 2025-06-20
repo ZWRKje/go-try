9050516079d2439134f7fd762418bd2777bd3b62
ZWRKje <andrery09767@gmail.com>
2025-06-20 20:52:19 +0300

    fix: package struct

--- 3-struct/bins/bins.go
+++ 3-struct/bins/bins.go
@@ -1,1 +1,27 @@
 package bins
+
+import "time"
+
+type Bin struct {
+	Id        string
+	Private   bool
+	CreatedAt time.Time
+	Name      string
+}
+
+type BinList struct {
+	bins []Bin
+}
+
+func NewBin(id string, private bool, createdAt time.Time, name string) *Bin {
+	return &Bin{
+		id,
+		private,
+		createdAt,
+		name,
+	}
+}
+
+func NewBinList() *BinList {
+	return &BinList{}
+}
--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,31 +1,10 @@
 package main
 
-import "time"
-
-type Bin struct {
-	Id        string
-	Private   bool
-	CreatedAt time.Time
-	Name      string
-}
-
-type BinList struct {
-	bins []Bin
-}
-
-func newBin(id string, private bool, createdAt time.Time, name string) *Bin {
-	return &Bin{
-		id,
-		private,
-		createdAt,
-		name,
-	}
-}
-
-func newBinList() *BinList {
-	return &BinList{}
-}
+import (
+	"struct/bins"
+	"time"
+)
 
 func main() {
-	_ = newBin("", false, time.Now(), "")
+	_ = bins.NewBin("", false, time.Now(), "")
 }
