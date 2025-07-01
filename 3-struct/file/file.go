a598e267fc93645ca5b328064599b8d67b3fde94
ZWRKje <andrery09767@gmail.com>
2025-07-01 21:37:28 +0300

    feat: add storage and files

--- 3-struct/file/file.go
+++ 3-struct/file/file.go
@@ -1,3 +1,43 @@
 package file
 
-func readFile() {}
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func ReadFile(name string) ([]byte, error) {
+	ext := filepath.Ext(name)
+
+	if strings.ToLower(ext) != ".json" {
+		return nil, errors.New("File extension must be json")
+	}
+
+	file, err := os.ReadFile(name)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func WriteFile(content []byte, name string) {
+	file, err := os.Create(name)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = file.Write(content)
+	if err != nil {
+		file.Close()
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Запись успешна")
+	file.Close()
+}
--- 3-struct/storage/storage.go
+++ 3-struct/storage/storage.go
@@ -1,3 +1,52 @@
 package storage
 
-func newStorage() {}
+import (
+	"encoding/json"
+	"struct/bins"
+	"struct/file"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+type Storage struct {
+	Bins      bins.BinList `json:"bins"`
+	UpdatedAt time.Time    `json:"updatedAt"`
+}
+
+func NewStorage() *Storage {
+	file, err := file.ReadFile("data.json")
+	if err != nil {
+		return &Storage{
+			Bins:      *bins.NewBinList(),
+			UpdatedAt: time.Now(),
+		}
+	}
+
+	var storage Storage
+
+	err = json.Unmarshal(file, &storage)
+	if err != nil {
+		color.Red("Не удалось разобрать файл data.json")
+	}
+
+	return &storage
+}
+
+func (st *Storage) ToBytes() ([]byte, error) {
+	file, err := json.Marshal(st)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func (st *Storage) SaveInfo() {
+	data, err := st.ToBytes()
+	if err != nil {
+		color.Red("Не удалось преобразовать")
+	}
+
+	file.WriteFile(data, "data.json")
+}
